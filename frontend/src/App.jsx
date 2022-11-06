9528cbf46092c6c44e3fd09aa9e03dfc9e0b2da9
Rohith N M <rohithnm02@gmail.com>
2022-11-06 11:42:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 import Forms from "./components/Forms";
 import RoomPage from "./pages/RoomPage";
 
-const server = "http://localhost:5000";
+const server = "https://railway.app";
 const connectionOptions = {
   "force new connection": true,
   reconnectionAttempts: "Infinity",
