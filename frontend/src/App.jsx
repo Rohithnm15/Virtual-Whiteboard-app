588bb057aa89c85bccab1b3a0c902769d23bda04
Rohith N M <rohithnm02@gmail.com>
2022-11-14 05:31:00 +0530

    Revert "Update App.jsx"
    This reverts commit 9528cbf46092c6c44e3fd09aa9e03dfc9e0b2da9.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 import Forms from "./components/Forms";
 import RoomPage from "./pages/RoomPage";
 
-const server = "https://railway.app";
+const server = "http://localhost:5000";
 const connectionOptions = {
   "force new connection": true,
   reconnectionAttempts: "Infinity",
