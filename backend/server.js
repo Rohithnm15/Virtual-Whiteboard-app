f8e3f56fd2d1531d8367d1a04b5160cc9aee47d0
Rohith N M <rohithnm02@gmail.com>
2024-07-24 02:07:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -68,5 +68,5 @@ io.on("connection", (socket) => {
 const port = process.env.PORT || 5000;
 
 server.listen(port, () =>
-  console.log("server is running on port ${port}")
+  console.log(`server is running on port ${port}`)
 );
